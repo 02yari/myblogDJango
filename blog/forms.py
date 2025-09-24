a02695e2738b93ba4b889bf2f90af896d140da7b
Yarielsy <yarigonza0294@gmail.com>
2025-09-24 04:54:10 -0500

    Primer commit: proyecto inicial con README

---
+++ blog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'content')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Tu nombre'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'tu@email.com'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Escribe tu comentario...'
+            }),
+        }
+        labels = {
+            'name': 'Nombre',
+            'email': 'Email',
+            'content': 'Comentario',
+        }
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(
+    template_name='blog/login.html',
+    next_page='blog:post_list'  # <- redirige al listado de posts
+), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+]
+
+
+
