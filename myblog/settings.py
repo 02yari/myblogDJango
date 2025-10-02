14bc2563a098c263597e1bb19de0a6c953707e23
Yarielsy <yarigonza0294@gmail.com>
2025-10-02 00:21:18 -0500

    Corregir variables de entorno de la base de datos: evitar error con quote_plus

--- myblog/settings.py
+++ myblog/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 #     }
 # }
 
-user = quote_plus(os.getenv("DB_USER"))
-password = quote_plus(os.getenv("DB_PASSWORD"))
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-name = os.getenv("DB_NAME")
+user = quote_plus(os.getenv("DB_USER", ""))
+password = quote_plus(os.getenv("DB_PASSWORD", ""))
+host = os.getenv("DB_HOST", "")
+port = os.getenv("DB_PORT", "")
+name = os.getenv("DB_NAME", "")
 
 DATABASES = {
     "default": dj_database_url.config(
