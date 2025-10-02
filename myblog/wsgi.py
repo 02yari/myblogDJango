3f812ad5912d1c45ec20c977ec511c62a0ffc0e3
Yarielsy <yarigonza0294@gmail.com>
2025-10-02 01:05:23 -0500

    Aplicar migraciones automáticamente en producción

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+import django
+from django.core.management import call_command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
+django.setup()
 
+# Ejecuta migraciones automáticamente al iniciar
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Error al aplicar migraciones: {e}")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
