2114f6323f0d4f0c20d64180628ecf36df8520be
Yarielsy <yarigonza0294@gmail.com>
2025-10-02 04:36:28 -0500

    Agrega modelo Profile con avatar y bio, señales para crear perfil automáticamente

--- blog/models.py
+++ blog/models.py
@@ -43,3 +43,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comentario de {self.name} en {self.post.title}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Relación 1 a 1 con User
+    bio = models.TextField(blank=True)                           # Biografía opcional
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')  # Imagen
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+# Señal para crear perfil automáticamente al registrar un usuario
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
