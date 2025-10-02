d0ae259327933472e9f6f5c940da6398ece39894
Yarielsy <yarigonza0294@gmail.com>
2025-10-02 04:48:35 -0500

    Crear formulario para editar perfil

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Comment
+from .models import Profile
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -34,3 +35,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'avatar']
